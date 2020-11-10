4d0ef7b09ba00ab326249e78d9fbb545ae149e18
James Sapp <jameseisapp@gmail.com>
2020-11-10 15:39:54 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "", :age => ""
         },
       :hero => {
         #your key/value pairs here
