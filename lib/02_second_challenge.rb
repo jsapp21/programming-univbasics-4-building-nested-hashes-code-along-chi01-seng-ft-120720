c51f3a8856b8258503682d9cc6d9fe720d30daf9
James Sapp <jameseisapp@gmail.com>
2020-11-10 15:37:16 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -4,11 +4,16 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => {}
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+    
+  }
 
   
 end
